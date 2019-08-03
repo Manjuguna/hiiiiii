4d5cde21ed59fc9012b76b69105c13274c69f899
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-08-03 14:09:39 +0530

    Update guu.py

--- guu.py
+++ guu.py
@@ -1,5 +1,5 @@
 saint=input()
-if(saint.find("GLGLGL")or saint.find("GRGRGR")or saint.find("GLLG")or saint,find("GRRG")):
+if(saint.find("GLGLGL")or saint.find("GRGRGR")or saint.find("GLLG")or saint.find("GRRG")):
 print("yes")
 else:
 print("no")
